70ad3a3200834a277e9bdcc836fdaee9d92f69d3
Jon Jagger <jon@jaggersoft.com>
2018-06-09 14:38:10 +0100

    Trying to fix build failure on Travis

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /\*\*\* (\d+) failure is detected/.match(output)
+  return :red   if /\*\*\* (\d+) failure detected in/.match(output)
   return :red   if /\*\*\* (\d+) failures are detected/.match(output)
   return :green if /\*\*\* No errors detected/.match(output)
   return :amber
