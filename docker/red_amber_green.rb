29ddb7e18c122f082f49d9effa6351e048ab9ee9
Jon Jagger <jon@jaggersoft.com>
2018-06-09 14:59:37 +0100

    fix red regexp pattern for multiple failing tests

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,8 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /\*\*\* (\d+) failure detected in/.match(output)
-  return :red   if /\*\*\* (\d+) failures are detected/.match(output)
+  return :red   if /\*\*\* (\d+) failure(s?) detected in/.match(output)
   return :green if /\*\*\* No errors detected/.match(output)
   return :amber
 }
