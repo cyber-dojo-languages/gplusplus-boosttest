5ff96e8cb55a8cd14861289b2458a13e1090e36e
Jon Jagger <jon@jaggersoft.com>
2018-06-13 07:22:01 +0100

    changed regex pattern for yet another change in the output

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /\*\*\* (\d+) failure(s?) detected in/.match(output)
+  return :red   if /\*\*\* (\d+) failure(s?) is detected in/.match(output)
   return :green if /\*\*\* No errors detected/.match(output)
   return :amber
 }
