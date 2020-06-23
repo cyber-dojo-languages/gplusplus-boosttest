bfcf12fb3b42cf1059b9e3cd727393c265a84f1f
Jon Jagger <jon@jaggersoft.com>
2020-06-23 09:45:59 +0100

    Update red-amber-green regex to match one or more test cases

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,8 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /\*\*\* (\d+) failure(s?) is detected in/.match(output)
+  return :red   if /\*\*\* 1 failure is detected in/.match(output)
+  return :red   if /\*\*\* (\d+) failures are detected in/.match(output)
   return :green if /\*\*\* No errors detected/.match(output)
   return :amber
 }
