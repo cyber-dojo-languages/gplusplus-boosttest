f12150a0a861f7c16c04ebaa08528627f39947a6
JonJagger <jon@kosli.com>
2026-02-05 11:54:08 +0000

    Upgrade to g++ 15.3.0, Boost.Test 1.83.0.2

---
+++ check_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -Eeu
+
+readonly REGEX="image_name\": \"(.*)\""
+readonly JSON=`cat docker/image_name.json`
+[[ ${JSON} =~ ${REGEX} ]]
+readonly IMAGE_NAME="${BASH_REMATCH[1]}"
+
+readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly EXPECTED=1.83.0.2
+readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} bash -c 'dpkg -s libboost-dev | grep Version')
+
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
+  echo "VERSION CONFIRMED as ${EXPECTED}"
+else
+  echo "VERSION EXPECTED: ${EXPECTED}"
+  echo "VERSION   ACTUAL: ${ACTUAL}"
+  exit 42
+fi
